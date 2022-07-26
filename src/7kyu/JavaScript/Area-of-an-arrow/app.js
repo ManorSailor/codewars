4d0a0153e64aa008afdf757b60f228f0b393eb86
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-07-26 18:55:08 +0530

    Add solution to Area-of-an-arrow

---
+++ src/7kyu/JavaScript/Area-of-an-arrow/app.js
@@ -0,0 +1,6 @@
+// Best practice | First attempt
+function arrowArea(a, b) {
+    // Area of Triangle: 1/2 * a * b.
+    // Divide the recatangle into two triangles, divide those two trianlges into 2 triangles
+    return (((a * b) / 2) / 2);
+}
