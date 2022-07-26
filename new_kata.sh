602a067cbfd816f80a65f669882f0e942273b68f
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2022-07-26 18:40:06 +0530

    feat: Script for generating structure for new kata
    I got tired of manually doing all this work. Thus, now there is shell Script
    which will do the work for me. Albeit, it is badly coded, I will fix it & improve
    as I go

---
+++ new_kata.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# This scripts generates the dummy template for storing my kata solutions
+# In its current stage, the script is really barebones & code can be removed immensely
+# What you see here is the MVP. Updates to this will come eventually
+
+# Check if the user provided a kata ID or not
+if [[ -z "$1" ]]; then
+    echo "You must provide a kata ID."
+    exit 1
+fi
+
+# CodeWars API URL
+readonly API="https://www.codewars.com/api/v1/code-challenges/$1"
+
+# Fetch the code challenge details from API
+data=$(curl "$API")
+
+# Check if API response returned a failure, exit the script
+[[ $(jq .success <<< $data) == "false" ]] && echo "Invalid kata ID" && exit 1
+
+echo $(jq .tags <<< "$data")
+
+# Store the required data in variables
+# TODO: Improve this shit pile of code
+name=$(jq .name <<< "$data" | tr -d "\'\"")
+slug=$(tr -s " " "-" <<< "$name")
+url=$(jq .url <<< "$data" | tr -d "\'\"")
+rank=$(jq .rank.name <<< "$data" | tr -d "\" ")
+description=$(jq -r .description <<< "$data" | tr -d "\'\"")
+tags=$(jq .tags <<< "$data" | tr -d "\"\n[] " | sed -E 's/,/, /g')
+creator=$(jq .createdBy.username <<< "$data" | tr -d "\'\"")
+creator_url=$(jq .createdBy.url <<< "$data" | tr -d "\'\"")
+
+# Directory path
+echo "${0%/}"
+
+# Create a directory for the kata
+mkdir -p ./"$rank"/"${foo:="JavaScript"}"/"${slug}"
+
+# Create a file called app.js in the target directory
+touch ./"$rank"/"${foo:="JavaScript"}"/"${slug}"/app.js
+
+# Generate a README using heredoc 
+cat <<README >> ./"$rank"/"${foo:="JavaScript"}"/"${slug}"/README.md
+## [$name]($url)
+---
+### Description
+
+$description
+
+<br>
+
+###### Tags: \`${tags[@]}\`
+
+###### Creator: [$creator]($creator_url)
+
+###### Completed On: \`$(date +%d-%m-%Y)\`
+
+###### Last Attempt On: \`$(date +%d-%m-%Y)\`
+
+---
+
+### Takeaways
+- Breaking the problem down on a paper is recommended
+README
